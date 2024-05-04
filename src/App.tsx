e503bd7fabcddac4952cb3b4d0335747ddbe32e7
amir <creator005@mail.ru>
2024-05-04 13:10:09 +0300

    gameAI

---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import "./App.css";
+import Move from "./logic/Minimax";
+
+function App() {
+  // P - Person, C - Computer
+  const [board, setBoard] = useState<(string | number)[]>([
+    0, 1, 2, 3, 4, 5, 6, 7, 8,
+  ]);
+  const [title, setTitle] = useState<string>("Крестики-Нолики");
+
+  function reset() {
+    setBoard([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+    setTitle("Крестики-Нолики");
+  }
+
+  const handleSetValue = (index: number, player: string) => {
+    if (board[index] !== "P" && board[index] !== "C") {
+      // setTimeout(() => {
+      board[index] = player;
+      setBoard([...board]);
+      player === "P" && Move(board, handleSetValue, reset, setTitle);
+      // }, 100);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        gap: "30px",
+        alignItems: "center",
+      }}
+    >
+      <h1
+        style={{
+          marginTop: "16vh",
+          fontSize: "34px",
+          backgroundColor: "white",
+          display: "inline-block",
+          padding: "8px",
+          borderRadius: "15px",
+        }}
+      >
+        {title}
+      </h1>
+      <div
+        style={{
+          width: "24vw",
+          minWidth: "300px",
+          display: "flex",
+          flexWrap: "wrap",
+        }}
+      >
+        {board.map((elem: number | string, index: number) => (
+          <button
+            key={index}
+            onClick={() => handleSetValue(index, "P")}
+            style={{
+              width: "33.3%",
+              height: "calc(24vw / 3.2)",
+              minHeight: "100px",
+              border: "4px solid black",
+              fontSize: "70px",
+              fontWeight: "800",
+              color: elem === "P" ? "red" : "blue",
+            }}
+          >
+            {elem === "P" ? "X" : elem === "C" && "O"}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/logic/Minimax.ts
@@ -0,0 +1,125 @@
+type IBoard = (string | number)[];
+
+const huPlayer = "P";
+const aiPlayer = "C";
+let round = 0;
+
+function Move(
+  board: IBoard,
+  handleSetValue: (index: number, player: string) => void,
+  reset: () => void,
+  setTitle: (newValue: string) => void
+) {
+  round++;
+  // console.log(board)
+  if (Winning(board, huPlayer)) {
+    setTimeout(() => {
+      round = 0;
+      reset();
+    }, 2500);
+    setTitle("Победа!");
+  } else if (round === 9) {
+    setTitle("Ничья!");
+    setTimeout(() => {
+      round = 0;
+      reset();
+    }, 2500);
+  } else {
+    const index: number = Minimax(board, aiPlayer)?.index as number;
+    handleSetValue(index, aiPlayer);
+    round++;
+    // console.log(index, round);
+    if (Winning(board, aiPlayer)) {
+      setTitle("Поражение!");
+      setTimeout(() => {
+        round = 0;
+        reset();
+      }, 2500);
+    } else if (round === 9) {
+      setTimeout(() => {
+        round = 0;
+        reset();
+      }, 2500);
+      setTitle("Ничья!");
+    }
+  }
+}
+
+function Minimax(board: IBoard, player: string) {
+  const availSlots: number[] = availSlotsFunc(board);
+  // console.log(availSlots)
+  if (Winning(board, huPlayer)) {
+    return {
+      score: -10,
+    };
+  } else if (Winning(board, aiPlayer)) {
+    return {
+      score: 10,
+    };
+  } else if (availSlots.length === 0) {
+    return {
+      score: 0,
+    };
+  }
+
+  const moves = [];
+  for (let i = 0; i < availSlots.length; i++) {
+    const move: { index?: number; score?: number } = {};
+    move.index = availSlots[i];
+    board[availSlots[i]] = player;
+
+    if (player === aiPlayer) {
+      const g = Minimax(board, huPlayer);
+      move.score = g?.score;
+    } else {
+      const g = Minimax(board, aiPlayer);
+      move.score = g?.score;
+    }
+
+    moves.push(move);
+    board[availSlots[i]] = move.index;
+    // console.log(moves)
+  }
+
+  let bestMove;
+  if (player === aiPlayer) {
+    let bestScore = -10000;
+    for (let elem of moves) {
+      if (elem.score != undefined && elem.score > bestScore) {
+        bestScore = elem.score;
+        bestMove = elem;
+      }
+    }
+  } else {
+    let bestScore = 10000;
+    for (let elem of moves) {
+      if (elem.score != undefined && elem.score < bestScore) {
+        bestScore = elem.score;
+        bestMove = elem;
+      }
+    }
+  }
+
+  return bestMove;
+}
+
+function Winning(board: IBoard, player: string): boolean {
+  if (
+    (board[0] == player && board[1] == player && board[2] == player) ||
+    (board[3] == player && board[4] == player && board[5] == player) ||
+    (board[6] == player && board[7] == player && board[8] == player) ||
+    (board[0] == player && board[3] == player && board[6] == player) ||
+    (board[1] == player && board[4] == player && board[7] == player) ||
+    (board[2] == player && board[5] == player && board[8] == player) ||
+    (board[0] == player && board[4] == player && board[8] == player) ||
+    (board[2] == player && board[4] == player && board[6] == player)
+  )
+    return true;
+  else return false;
+}
+
+function availSlotsFunc(board: IBoard): number[] {
+  return board.filter((elem) => elem !== "P" && elem !== "C") as number[];
+}
+
+export default Move;
